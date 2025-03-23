dd7912e58f0095d1bfa72763a3a3b60fd5915671
barkais <39986943+barkais@users.noreply.github.com>
2025-03-23 18:11:30 +0200

    Add files via upload

---
+++ R/data_doc.R
@@ -0,0 +1,25 @@
+## data documentation
+
+#' example_prediction_data
+#'
+#' A prediction example dataset
+#' @keywords internal
+#' @name example_prediction_data
+#' @docType data
+NULL
+
+#' example_training_data
+#'
+#' A training example dataset
+#' @keywords internal
+#' @name example_training_data
+#' @docType data
+NULL
+
+#' example_validation_data
+#'
+#' A validation example dataset
+#' @keywords internal
+#' @name example_validation_data
+#' @docType data
+NULL
